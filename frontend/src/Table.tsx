1c3ccb35ce9d8c6ed9c5803c456c1e1422cb1e70
Krona Emmanuel <kronaemmanuel@gmail.com>
2022-06-21 05:20:05 +0500

    Add search filter to Table

--- frontend/src/Table.tsx
+++ frontend/src/Table.tsx
@@ -1,66 +1,82 @@
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+
 import {policy} from "./interfaces/policy.interface"
 import Badge from "./Badge";
 
-const Table = () => {
+const Table: React.FC<{}> = () => {
+  const [searchValue, setSearchValue] = useState<string>("")
+
   const [policies, setPolicies] = useState<policy[]>([])
-  // Get data from the backend
   useEffect(() => {
-    fetch("http://localhost:4000/policies")
+    fetch(`http://localhost:4000/policies?search=${searchValue}`)
       .then(response => response.json())
       .then(jsonData => setPolicies(jsonData))
-  }, [])
+  }, [searchValue])
 
   return (
-    <div className="flex flex-col">
-      <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="overflow-hidden rounded-lg shadow-sm">
-            <table className="min-w-full">
-              <thead className="border-b bg-gray-100">
-                <tr>
-                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                    #
-                  </th>
-                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                    Name
-                  </th>
-                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                    Provider
-                  </th>
-                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                    Type
-                  </th>
-                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                    Status
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {policies &&
-                  policies.map((policy, index) => {
-                    return (
-                      <tr className="border-b" key={policy.id}>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                          {index + 1}
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          {policy.customer.firstName} {policy.customer.lastName}
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          {policy.provider}
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          {policy.insuranceType}
-                        </td>
-                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                          <Badge status={policy.status} />
-                        </td>
-                      </tr>
-                    )
-                  })}
-              </tbody>
-            </table>
+    <div>
+      <div>
+        <form>
+          <label htmlFor="searchValue" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+          <div className="relative">
+            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+              <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            </div>
+            <input onChange={e => setSearchValue(e.target.value)} value={searchValue} type="text" id="searchValue" name="searchValue" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search policies" />
+            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          </div>
+        </form>
+      </div>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden rounded-lg shadow-sm">
+              <table className="min-w-full">
+                <thead className="border-b bg-gray-100">
+                  <tr>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      #
+                    </th>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Name
+                    </th>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Provider
+                    </th>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Type
+                    </th>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {policies &&
+                    policies.map((policy, index) => {
+                      return (
+                        <tr className="border-b" key={policy.id}>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {index + 1}
+                          </td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {policy.customer.firstName} {policy.customer.lastName}
+                          </td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {policy.provider}
+                          </td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            {policy.insuranceType}
+                          </td>
+                          <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                            <Badge status={policy.status} />
+                          </td>
+                        </tr>
+                      )
+                    })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
